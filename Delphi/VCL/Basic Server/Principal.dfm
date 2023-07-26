e8a1b1b29822bdff663c9d74a6ed9c5a8caf85cf
Mobius One <mobius1qwe@yahoo.com.br>
2023-07-26 19:48:15 -0300

    * Ajustes no Basic Server pra incluir listagem das rotas e log

--- Delphi/VCL/Basic Server/Principal.dfm
+++ Delphi/VCL/Basic Server/Principal.dfm
@@ -2,15 +2,37 @@ object fPrincipal: TfPrincipal
   Left = 0
   Top = 0
   Caption = 'RAL Basic Server'
-  ClientHeight = 75
-  ClientWidth = 268
+  ClientHeight = 203
+  ClientWidth = 403
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  OnCreate = FormCreate
   TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 53
+    Width = 34
+    Height = 13
+    Caption = 'Routes'
+  end
+  object Label2: TLabel
+    Left = 119
+    Top = 53
+    Width = 17
+    Height = 13
+    Caption = 'Log'
+  end
+  object lServerPath: TLabel
+    Left = 102
+    Top = 8
+    Width = 54
+    Height = 13
+    Caption = 'ServerPath'
+  end
   object ToggleSwitch1: TToggleSwitch
     Left = 8
     Top = 8
@@ -19,27 +41,74 @@ object fPrincipal: TfPrincipal
     TabOrder = 0
     OnClick = ToggleSwitch1Click
   end
+  object Memo2: TMemo
+    Left = 119
+    Top = 72
+    Width = 282
+    Height = 130
+    Lines.Strings = (
+      'Memo2')
+    TabOrder = 1
+  end
+  object ListView1: TListView
+    Left = 8
+    Top = 72
+    Width = 105
+    Height = 130
+    Columns = <>
+    TabOrder = 2
+    ViewStyle = vsList
+  end
+  object RALBasicAuth: TRALServerBasicAuth
+    AuthDialog = True
+    UserName = 'testeuser'
+    Password = 'testepass'
+    Left = 184
+    Top = 112
+  end
   object Server: TRALIndyServer
     Active = False
+    Authentication = RALBasicAuth
+    BruteForceProtection.Enabled = True
+    BruteForceProtection.ExpirationMin = 30
+    BruteForceProtection.MaxTry = 3
     Port = 8000
     Routes = <
       item
-        DisplayName = 'ping'
-        Document = 'ping'
-        RouteList = <>
+        RouteDomain = '/'
+        RouteName = 'ping'
         AllowedMethods = [amALL]
         SkipAuthMethods = []
         Callback = False
         OnReply = ping
+      end
+      item
+        RouteDomain = '/'
+        RouteName = 'test'
+        AllowedMethods = [amALL]
+        SkipAuthMethods = [amALL]
+        Callback = False
+        OnReply = test
       end>
     ServerStatus.Strings = (
       '<html>'
+      '<head>'
+      '<title>RALServer - $ralversion;</title>'
+      '</head>'
       '<body>'
       '<h1>Server OnLine</h1>'
+      '<h4>Version: $ralversion;</h4>'
+      '<h4>Engine: $ralengine;</h4>'
       '</body>'
       '</html>')
+    SessionTimeout = 0
     ShowServerStatus = True
-    Left = 129
-    Top = 8
+    SSL.Enabled = False
+    SSL.SSLOptions.Mode = sslmUnassigned
+    SSL.SSLOptions.VerifyMode = []
+    SSL.SSLOptions.VerifyDepth = 0
+    OnClientRequest = Log
+    Left = 56
+    Top = 104
   end
 end
--- Delphi/VCL/Basic Server/Principal.pas
+++ Delphi/VCL/Basic Server/Principal.pas
@@ -7,16 +7,28 @@ uses
   System.SysUtils, System.Variants, System.Classes,
   Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,
   Vcl.WinXCtrls,
-  RALServer, RALRoutes,  RALIndyServer, RALTypes;
+  RALServer, RALRoutes, RALRequest, RALResponse, RALParams, RALIndyServer,
+  RALTypes, RALAuthentication, RALConsts, Vcl.ComCtrls;
 
 type
   TfPrincipal = class(TForm)
-    Server: TRALIndyServer;
     ToggleSwitch1: TToggleSwitch;
+    RALBasicAuth: TRALServerBasicAuth;
+    Server: TRALIndyServer;
+    Memo2: TMemo;
+    Label1: TLabel;
+    Label2: TLabel;
+    ListView1: TListView;
+    lServerPath: TLabel;
     procedure poolerrclientesReply(Sender: TObject; ARequest: TRALRequest;
       var AResponse: TRALResponse);
     procedure ToggleSwitch1Click(Sender: TObject);
     procedure ping(Sender: TObject; ARequest: TRALRequest;
+      var AResponse: TRALResponse);
+    procedure test(Sender: TObject; ARequest: TRALRequest;
+      var AResponse: TRALResponse);
+    procedure FormCreate(Sender: TObject);
+    procedure Log(Sender: TObject; ARequest: TRALRequest;
       var AResponse: TRALResponse);
   private
     { Private declarations }
@@ -31,11 +43,11 @@ implementation
 
 {$R *.dfm}
 
-procedure TfPrincipal.poolerrclientesReply(Sender: TObject; ARequest: TRALRequest;
-  var AResponse: TRALResponse);
+procedure TfPrincipal.poolerrclientesReply(Sender: TObject;
+  ARequest: TRALRequest; var AResponse: TRALResponse);
 var
-  nome : string;
-  vParam : TRALParam;
+  nome: string;
+  vParam: TRALParam;
 begin
   nome := 'erro';
   vParam := ARequest.Params.ParamName['nome'];
@@ -45,8 +57,44 @@ begin
   AResponse.ResponseText := nome;
 end;
 
-procedure TfPrincipal.ping(Sender: TObject; ARequest: TRALRequest;
+procedure TfPrincipal.test(Sender: TObject; ARequest: TRALRequest;
   var AResponse: TRALResponse);
+begin
+  AResponse.ContentType := 'text/plain';
+  AResponse.ResponseText := 'pong';
+  case ARequest.Method of
+    amGET, amDELETE:
+      AResponse.RespCode := 200;
+    amPOST, amPUT, amPATCH:
+      AResponse.RespCode := 201;
+  end;
+end;
+
+procedure TfPrincipal.FormCreate(Sender: TObject);
+var
+  I: Integer;
+begin
+  ListView1.Clear;
+  for I := 0 to pred(Server.Routes.Count) do
+    ListView1.Items.Add.Caption := TRALRoute(Server.Routes.Items[I]).RouteName;
+end;
+
+procedure TfPrincipal.Log(Sender: TObject; ARequest: TRALRequest;
+  var AResponse: TRALResponse);
+var
+  response: string;
+begin
+  response := AResponse.ResponseText;
+  TThread.Queue(nil,
+    procedure
+    begin
+      Memo2.Lines.Append('REQUEST: ' + ARequest.Query);
+      Memo2.Lines.Append('RESPONSE: ' + response);
+    end);
+end;
+
+procedure TfPrincipal.ping(Sender: TObject; ARequest: TRALRequest;
+var AResponse: TRALResponse);
 begin
   // No http verb is checked here, so it'll answer the same to any incomming
   // This is a simple GET, POST, PUT, PATCH, DELETE request against /ping endpoint
@@ -57,6 +105,10 @@ end;
 procedure TfPrincipal.ToggleSwitch1Click(Sender: TObject);
 begin
   Server.Active := ToggleSwitch1.State = tssOn;
+  if Server.Active then
+    lServerPath.Caption := 'http://localhost:' + Server.Port.ToString
+  else
+    lServerPath.Caption := 'Offline';
 end;
 
 end.
