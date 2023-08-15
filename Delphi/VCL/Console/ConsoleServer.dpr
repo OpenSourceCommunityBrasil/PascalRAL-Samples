a896a733b0140c526001c939f1aebc3527b90c86
Mobius One <mobius1qwe@yahoo.com.br>
2023-08-15 17:08:21 -0300

    + Demo ConsoleServer

---
+++ Delphi/VCL/Console/ConsoleServer.dpr
@@ -0,0 +1,29 @@
+program ConsoleServer;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils,
+  RALServer,
+  RALSynopseServer,
+  RALRequest,
+  RALResponse,
+  Rotas in 'Rotas.pas';
+
+var
+  Server: TRALServer;
+
+begin
+  Server := TRALSynopseServer.Create(nil);
+  Server.Port := 8000;
+  Server.ShowServerStatus := true;
+  Rota.CreateRoute(Server);
+
+  Server.Active := true;
+  Writeln('press any key to close the app...');
+  Readln;
+  Server.Active := false;
+  FreeAndNil(Server);
+
+end.
---
+++ Delphi/VCL/Console/Rotas.pas
@@ -0,0 +1,41 @@
+unit Rotas;
+
+interface
+
+uses
+  SysUtils,
+  RALServer, RALRequest, RALResponse, RALMIMETypes, RALConsts;
+
+Type
+  TRotas = class
+  private
+    procedure Console(Sender: TObject; ARequest: TRALRequest; AResponse: TRALResponse);
+    procedure Default(Sender: TObject; ARequest: TRALRequest; AResponse: TRALResponse);
+  public
+    procedure CreateRoute(AServer: TRALServer);
+  end;
+
+var
+  Rota: TRotas;
+
+implementation
+
+{ TRotas }
+
+procedure TRotas.Console(Sender: TObject; ARequest: TRALRequest; AResponse: TRALResponse);
+begin
+  AResponse.Answer(200, 'RALServer console mode', rctTEXTPLAIN);
+end;
+
+procedure TRotas.CreateRoute(AServer: TRALServer);
+begin
+  AServer.CreateRoute('console', Console);
+  AServer.CreateRoute('', Default);
+end;
+
+procedure TRotas.Default(Sender: TObject; ARequest: TRALRequest; AResponse: TRALResponse);
+begin
+  AResponse.Answer(200, RALDefaultPage, rctTEXTHTML);
+end;
+
+end.
