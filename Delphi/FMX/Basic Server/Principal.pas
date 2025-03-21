2ba900851d8129633b776b5c06b5dcc7578d458f
Mobius One <mobius1qwe@gmail.com>
2025-03-21 19:50:59 -0300

    - Atualização de exemplo Delphi/FMX/Basic Server

--- Delphi/FMX/Basic Server/Principal.pas
+++ Delphi/FMX/Basic Server/Principal.pas
@@ -5,10 +5,12 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
+
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.ListView.Types,
   FMX.ListView.Appearances, FMX.ListView.Adapters.Base, FMX.Memo.Types, FMX.ScrollBox,
   FMX.Memo, FMX.ListView, FMX.StdCtrls, FMX.Controls.Presentation, FMX.Edit,
-  RALTypes,
+
+  RALTypes, RALConsts,
   RALServer, RALIndyServer, RALCustomObjects, RALRequest, RALResponse;
 
 type
@@ -17,11 +19,14 @@ type
     Label1: TLabel;
     Switch1: TSwitch;
     lServerInfo: TLabel;
-    ListView1: TListView;
     Memo1: TMemo;
     RALIndyServer1: TRALIndyServer;
+    Label2: TLabel;
+    Memo2: TMemo;
     procedure RALIndyServer1Routes_pingReply(ARequest: TRALRequest;
       AResponse: TRALResponse);
+    procedure Switch1Switch(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     { Private declarations }
   public
@@ -35,10 +40,30 @@ implementation
 
 {$R *.fmx}
 
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  // stop the server before killing the application to prevent socket errors
+  RALIndyServer1.Stop;
+end;
+
 procedure TForm1.RALIndyServer1Routes_pingReply(ARequest: TRALRequest;
   AResponse: TRALResponse);
 begin
-  AResponse.Answer(200, 'pong');
+  // HTTP_OK is a constant on RALConsts
+  // This is a minimal example on how to reply a request with a simple text/plain
+  AResponse.Answer(HTTP_OK, 'pong');
+end;
+
+procedure TForm1.Switch1Switch(Sender: TObject);
+begin
+  // Change activation state of the server based on the TSwitch state
+  RALIndyServer1.Active := Switch1.IsChecked;
+  // you can also use RALServer.Start; or RALServer.Stop; methods to start/stop
+  // the server.
+
+  // List all routes of the server on Memo2
+  if Switch1.IsChecked then
+    Memo2.Lines.Text := RALIndyServer1.Routes.AsString;
 end;
 
 end.
